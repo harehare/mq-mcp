0248ec02b1409c6ad98b08232d6664a2a8bd3f56
harehare <harehare1110@gmail.com>
2025-12-15 22:24:13 +0900

    📝 Fix: update dependencies in Cargo.toml and enhance tracing subscriber configuration in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 pub mod server;
 
+use tracing_subscriber::EnvFilter;
+
 #[tokio::main]
 async fn main() -> miette::Result<()> {
-    tracing_subscriber::fmt().init();
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .with_thread_names(true)
+        .with_target(true)
+        .with_line_number(true)
+        .init();
+
     server::start().await
 }
